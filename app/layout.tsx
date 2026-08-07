0bd6880c5b6f9a931d01717a9800ad6a59058002
zubii <meharzubair703@gmail.com>
2026-08-07 03:30:08 +0300

    Revert "chore: enforce dynamic rendering to fix vercel build"
    This reverts commit c175723b3c83f307d44c965e60257d1ac950e993.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
