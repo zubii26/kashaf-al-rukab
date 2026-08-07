c175723b3c83f307d44c965e60257d1ac950e993
zubii <meharzubair703@gmail.com>
2026-08-07 03:24:55 +0300

    chore: enforce dynamic rendering to fix vercel build

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
